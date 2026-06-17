a1cc82e9092214b70a699607c17b056625068563
SanjanaJanardhan <sanjanardh19@gmail.com>
2026-06-17 12:28:11 -0700

    refactor: move MongoDB URI to environment variable

--- SereneSpace_Node/server.js
+++ SereneSpace_Node/server.js
@@ -12,7 +12,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-mongoose.connect('mongodb://127.0.0.1:27017/SereneSpace', {
+mongoose.connect(process.env.MONGO_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
 });
